0cacfd7cb47d493d983e3bb6a428663e16ad81b1
Libor Rysavy <liry@users.noreply.github.com>
2019-02-01 14:09:31 +0100

    Update dependencies

--- src/test/java/com/gooddata/http/client/LoginSSTRetrievalStrategyTest.java
+++ src/test/java/com/gooddata/http/client/LoginSSTRetrievalStrategyTest.java
@@ -38,8 +38,8 @@ import static org.hamcrest.Matchers.containsString;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.isA;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
